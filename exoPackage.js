1392f314e48e344027207e3985462ab40fd559ff
Aidan Ranson <35737617+NightFell999@users.noreply.github.com>
2023-04-02 21:36:07 -0400

    Add files via upload

---
+++ exoPackage.js
@@ -0,0 +1,11 @@
+function before(){
+    document.getElementById('MOON')
+    .src="asteroid.png";
+}
+      
+function afterr(){
+    document.getElementById('myImage')
+    .src="img/photo2.jpg";
+    document.getElementById('message')
+    .innerHTML="Bye! GeeksforGeeks people";
+}
