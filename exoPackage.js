3be6e3e89980463f0c9a07eda815e54211358a2f
BluePepper31882 <51997915+BluePepper31882@users.noreply.github.com>
2023-04-03 09:54:03 -0400

    Add files via upload

--- exoPackage.js
+++ exoPackage.js
@@ -5,8 +5,28 @@ function moon(){
 }
       
 function venus(){
+    
+    if (document.getElementById('earth').className = "Image") {
+        document.getElementById('earth').className = "fadeIn";
+    }
+    else {
     document.getElementById('earth').className = "fadeIn";
     document.getElementById('MOON').className = "fadeOut";
+    }
 
+}
+
+
+var money;
+function week1(){
+    money = 4000;
+}
+
+function week2(){
+    money = 5000;
+}
+
+function week3(){
+    money = 6000;
 }
 
